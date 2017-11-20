a976e54397a8de29006d916133349cce83084da3
James Stack <james.a.stack@gmail.com>
2017-11-19 18:23:10 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res) => {
+
+  if (!ObjectID.isValid(req.params.id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    if (todo){
+      res.send({todo});
+    } else {
+      res.status(404).send();
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
